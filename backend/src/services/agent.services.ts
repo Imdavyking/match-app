fa23b6781f3df8defa5556560c4bf25a4bef5c5b
imdavyking <yungdynamic53@gmail.com>
2025-02-08 10:44:35 +0100

    chore: updated

--- backend/src/services/agent.services.ts
+++ backend/src/services/agent.services.ts
@@ -18,7 +18,7 @@ const tools: Tool[] = [
         tools: ["createRequestAI"],
       },
     ],
-    call: async (input: Promise<Data>) => {
+    call: async (input: Promise<Data>): Promise<Data> => {
       const data = await input;
 
       const { name, description } = JSON.parse(data.toString());
@@ -33,150 +33,154 @@ const tools: Tool[] = [
       });
     },
   },
-  // {
-  //   name: "getRequestAI",
-  //   description: "Fetch a request by ID.",
-  //   examples: [
-  //     {
-  //       prompt: "Get details for request ID 123.",
-  //       code: `const output = getRequestAI({ id: '123' })`,
-  //       tools: ["getRequestAI"],
-  //     },
-  //   ],
-  //   call: async ({ id }: { id: string }) => {
-  //     return {
-  //       name: "getRequestAI",
-  //       args: {
-  //         id,
-  //       },
-  //       type: "tool_call",
-  //       id: 2,
-  //     };
-  //   },
-  // },
-  // {
-  //   name: "createStoreAI",
-  //   description: "Create a new store.",
-  //   examples: [
-  //     {
-  //       prompt:
-  //         "Create a store with name Kingsmen and description we are best.",
-  //       code: `const output = createStoreAI({ name: 'Kingsmen', description: 'we are best' })`,
-  //       tools: ["createStoreAI"],
-  //     },
-  //   ],
-  //   call: async ({
-  //     name,
-  //     description,
-  //   }: {
-  //     name: string;
-  //     description: string;
-  //   }) => {
-  //     return {
-  //       name: "createStoreAI",
-  //       args: {
-  //         name,
-  //         description,
-  //       },
-  //       type: "tool_call",
-  //       id: 3,
-  //     };
-  //   },
-  // },
-  // {
-  //   name: "createUserAI",
-  //   description: "Create a new user.",
-  //   examples: [
-  //     {
-  //       prompt:
-  //         "Create a new buyer user with name john_doe and phone 1234567890.",
-  //       code: `const output = createUserAI({ username: 'john_doe', account_type: 'buyer', phone: '1234567890' })`,
-  //       tools: ["createUserAI"],
-  //     },
-  //   ],
+  {
+    name: "getRequestAI",
+    description: "Fetch a request by ID.",
+    examples: [
+      {
+        prompt: "Get details for request ID 123.",
+        code: `const output = getRequestAI('{ id: "123" }')`,
+        tools: ["getRequestAI"],
+      },
+    ],
+    call: async (input: Promise<Data>): Promise<Data> => {
+      const data = await input;
 
-  //   call: async ({
-  //     username,
-  //     account_type,
-  //     phone,
-  //   }: {
-  //     username: string;
-  //     account_type: string;
-  //     phone: string;
-  //   }) => {
-  //     return {
-  //       name: "createUserAI",
-  //       args: {
-  //         username,
-  //         account_type,
-  //         phone,
-  //       },
-  //       type: "tool_call",
-  //       id: 4,
-  //     };
-  //   },
-  // },
-  // {
-  //   name: "updateUserAI",
-  //   description: "Update an existing user.",
-  //   examples: [
-  //     {
-  //       prompt: "Update user information.",
-  //       code: `const output = updateUserAI({})`,
-  //       tools: ["updateUserAI"],
-  //     },
-  //   ],
-  //   call: async ({}) => {
-  //     return {
-  //       name: "updateUserAI",
-  //       args: {},
-  //       type: "tool_call",
-  //       id: 5,
-  //     };
-  //   },
-  // },
-  // {
-  //   name: "toggleLocationAI",
-  //   description: "Enable or disable location tracking.",
-  //   examples: [
-  //     {
-  //       prompt: "Enable location tracking.",
-  //       code: `const output = toggleLocationAI({ isEnabled: true })`,
-  //       tools: ["toggleLocationAI"],
-  //     },
-  //   ],
-  //   call: async ({ isEnabled }: { isEnabled: boolean }) => {
-  //     return {
-  //       name: "toggleLocationAI",
-  //       args: {
-  //         isEnabled,
-  //       },
-  //       type: "tool_call",
-  //       id: 6,
-  //     };
-  //   },
-  // },
-  // {
-  //   name: "fetchUserByIdAI",
-  //   description: "Fetch user details by ID.",
-  //   examples: [
-  //     {
-  //       prompt: "Get details for user ID 456.",
-  //       code: `const output = fetchUserByIdAI({ id: '456' })`,
-  //       tools: ["fetchUserByIdAI"],
-  //     },
-  //   ],
-  //   call: async ({ id }: { id: string }) => {
-  //     return {
-  //       name: "fetchUserByIdAI",
-  //       args: {
-  //         id,
-  //       },
-  //       type: "tool_call",
-  //       id: 7,
-  //     };
-  //   },
-  // },
+      const { id } = JSON.parse(data.toString());
+      return JSON.stringify({
+        name: "getRequestAI",
+        args: {
+          id,
+        },
+        type: "tool_call",
+        id: 2,
+      });
+    },
+  },
+  {
+    name: "createStoreAI",
+    description: "Create a new store.",
+    examples: [
+      {
+        prompt:
+          "Create a store with name Kingsmen and description we are best.",
+        code: `const output = createStoreAI('{"name":"Kingsmen","description":"we are best"}')`,
+        tools: ["createStoreAI"],
+      },
+    ],
+    call: async (input: Promise<Data>): Promise<Data> => {
+      const data = await input;
+
+      const { name, description } = JSON.parse(data.toString());
+      return JSON.stringify({
+        name: "createStoreAI",
+        args: {
+          name,
+          description,
+        },
+        type: "tool_call",
+        id: 3,
+      });
+    },
+  },
+  {
+    name: "createUserAI",
+    description: "Create a new user.",
+    examples: [
+      {
+        prompt:
+          "Create a new buyer user with name john_doe and phone 1234567890.",
+        code: `const output = createUserAI('{"username":"john_doe","account_type":"buyer","phone":"1234567890"}')`,
+        tools: ["createUserAI"],
+      },
+    ],
+
+    call: async (input: Promise<Data>): Promise<Data> => {
+      const data = await input;
+
+      const { username, account_type, phone } = JSON.parse(data.toString());
+      return JSON.stringify({
+        name: "createUserAI",
+        args: {
+          username,
+          account_type,
+          phone,
+        },
+        type: "tool_call",
+        id: 4,
+      });
+    },
+  },
+  {
+    name: "updateUserAI",
+    description: "Update an existing user.",
+    examples: [
+      {
+        prompt: "Update user information.",
+        code: `const output = updateUserAI({})`,
+        tools: ["updateUserAI"],
+      },
+    ],
+    call: async (input: Promise<Data>): Promise<Data> => {
+      const data = await input;
+
+      const { _ } = JSON.parse(data.toString());
+      return JSON.stringify({
+        name: "updateUserAI",
+        args: {},
+        type: "tool_call",
+        id: 5,
+      });
+    },
+  },
+  {
+    name: "toggleLocationAI",
+    description: "Enable or disable location tracking.",
+    examples: [
+      {
+        prompt: "Enable location tracking.",
+        code: `const output = toggleLocationAI('{ "isEnabled": true }')`,
+        tools: ["toggleLocationAI"],
+      },
+    ],
+    call: async (input: Promise<Data>): Promise<Data> => {
+      const data = await input;
+
+      const { isEnabled } = JSON.parse(data.toString());
+      return JSON.stringify({
+        name: "toggleLocationAI",
+        args: {
+          isEnabled,
+        },
+        type: "tool_call",
+        id: 6,
+      });
+    },
+  },
+  {
+    name: "fetchUserByIdAI",
+    description: "Fetch user details by ID.",
+    examples: [
+      {
+        prompt: "Get details for user ID 456.",
+        code: `const output = fetchUserByIdAI('{ id: "456" }')`,
+        tools: ["fetchUserByIdAI"],
+      },
+    ],
+    call: async (input: Promise<Data>): Promise<Data> => {
+      const data = await input;
+
+      const { id } = JSON.parse(data.toString());
+      return JSON.stringify({
+        name: "fetchUserByIdAI",
+        args: {
+          id,
+        },
+        type: "tool_call",
+        id: 7,
+      });
+    },
+  },
 ];
 
 const api = new OpenAIApi(
@@ -188,8 +192,6 @@ const llmOpenAI = async (prompt: string): Promise<string> => {
     model: "gpt-4o-mini",
     messages: [{ role: "user", content: prompt }],
   });
-
-  console.log({ choices: completion.data.choices[0].message });
 
   if (completion.data.choices[0]?.message?.content) {
     return completion.data.choices[0].message.content;
@@ -207,9 +209,12 @@ export async function runAIAgent(messages: string) {
 
     const response = await agent.run(messages);
 
-    console.log(response);
+    const results = [];
+    for (const res of response) {
+      results.push(await res.data);
+    }
 
-    return response;
+    return results;
   } catch (error) {
     console.error(error);
     return {};
